0b54fcc01f7f0afa97662e0932588ca11806725b
mjbza <mahdi.jaberzadehansa@ucalgary.ca>
2022-12-08 15:46:16 -0700

    An stable version

--- XMLViewerApplication/MainWindow.xaml.cs
+++ XMLViewerApplication/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml;
 
 namespace XMLViewerApplication
 {
@@ -23,6 +24,36 @@ namespace XMLViewerApplication
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void BrowseXmlFile(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog openFileDialog = new Microsoft.Win32.OpenFileDialog();
+            openFileDialog.CheckFileExists = true;
+            openFileDialog.Filter = "XML Files (*.xml)|*.xml|All Files(*.*)|*.*";
+            openFileDialog.Multiselect = false;
+
+            if (openFileDialog.ShowDialog() != true) { return; }
+
+            XmlDocument XMLdoc = new XmlDocument();
+            try
+            {
+                XMLdoc.Load(openFileDialog.FileName);
+            }
+            catch (XmlException)
+            {
+                MessageBox.Show("The XML file is invalid");
+                return;
+            }
+
+            textBoxFilePath.Text = openFileDialog.FileName;
+            vXMLViwer.XmlDocument = XMLdoc;
+        }
+
+        private void ClearXmlFile(object sender, RoutedEventArgs e)
+        {
+            textBoxFilePath.Text = string.Empty;
+            vXMLViwer.XmlDocument = null;
         }
     }
 }
