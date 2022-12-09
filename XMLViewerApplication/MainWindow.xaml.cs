3af89e668b764a4af345c46c85e0fd5442620f8e
mjbza <mahdi.jaberzadehansa@ucalgary.ca>
2022-12-08 21:56:13 -0700

    improvement on code

--- XMLViewerApplication/MainWindow.xaml.cs
+++ XMLViewerApplication/MainWindow.xaml.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Diagnostics;
 using System.Windows;
 using System.Xml;
 using XMLViewer;
@@ -11,25 +10,39 @@ namespace XMLViewerApplication
     /// </summary>
     public partial class MainWindow : Window
     {
-        private readonly String _appTitle = "XML Viewer";
+        /// <summary>
+        /// The title of the application
+        /// </summary>
+        private readonly string _appTitle = "XML Viewer";
+
+        /// <summary>
+        /// The constructor. 
+        /// </summary>
         public MainWindow()
         {
             InitializeComponent();
-            this.Title = _appTitle;
+            Title = _appTitle;
             vXMLViwer.CalculateSelectedNodePath = true;
         }
 
-        
+        /// <summary>
+        /// When user presses the Open menu, this event handler is called. 
+        /// It opens the file dialog.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void MenuOpen_Click(object sender, RoutedEventArgs e)
         {
-            Microsoft.Win32.OpenFileDialog openFileDialog = new Microsoft.Win32.OpenFileDialog();
-            openFileDialog.CheckFileExists = true;
-            openFileDialog.Filter = "XML Files (*.xml)|*.xml|All Files(*.*)|*.*";
-            openFileDialog.Multiselect = false;
+            Microsoft.Win32.OpenFileDialog openFileDialog = new()
+            {
+                CheckFileExists = true,
+                Filter = "XML Files (*.xml)|*.xml|All Files(*.*)|*.*",
+                Multiselect = false
+            };
 
             if (openFileDialog.ShowDialog() != true) { return; }
 
-            XmlDocument XMLdoc = new XmlDocument();
+            XmlDocument XMLdoc = new();
             try
             {
                 XMLdoc.Load(openFileDialog.FileName);
@@ -39,30 +52,51 @@ namespace XMLViewerApplication
                 MessageBox.Show("The XML file is invalid");
                 return;
             }
-            this.Title = _appTitle + openFileDialog.SafeFileName;
+            Title = _appTitle + openFileDialog.SafeFileName;
             vXMLViwer.XmlDocument = XMLdoc;
         }
 
+        /// <summary>
+        /// When user presses the Close menu, this event handler is called. 
+        /// It closes the file.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void MenuClose_Click(object sender, RoutedEventArgs e)
         {
-            this.Title = _appTitle;
+            Title = _appTitle;
             textBlockFilePath.Text = string.Empty;
             vXMLViwer.XmlDocument = null;
         }
 
+        /// <summary>
+        /// When user presses the Exit menu, this event handler is called. 
+        /// It terminates the program.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void MenuExit_Click(object sender, RoutedEventArgs e)
         {
-            this.MenuClose_Click(sender,e);
+            // Make sure the file has been closed before exit
+            MenuClose_Click(sender,e);
             Application.Current.Shutdown();
         }
 
+        /// <summary>
+        /// Event handler for when user select a node in the tree view. It shows the path in the view. 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void XMLViwer_SelectedItemChanged(object sender, EventArgs e)
         {
-            SelectedItemChangedEventArgs? selectedItemChangedEventArgs = e as SelectedItemChangedEventArgs;
-            if (selectedItemChangedEventArgs != null)
+            if (e is SelectedItemChangedEventArgs selectedItemChangedEventArgs)
             {
                 textBlockFilePath.Text = "Path: " + selectedItemChangedEventArgs.SelectedNodePath;
-            }            
+            }
+            else
+            {
+                textBlockFilePath.Text = string.Empty;
+            }
         }
     }
 }
